6bfd95d974e6b94c95090c8bf7eafc2b725e00aa
ngzax <ngzax@comcast.net>
2011-06-21 09:37:33 -0400

    Added generic example of LDAP for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'imkhJ6VxilZIA7AO36XpHg', 'FsFr1yuVI7RMp9O7v97j8dHkwSfFKeLMcgK74hyys', :sign_in => true
 end
+
+# use OmniAuth::Strategies::LDAP, :host => '10.101.10.1', :port => 389, :method => :plain, :base => 'dc=intridea, dc=com', :uid => 'sAMAccountName'
