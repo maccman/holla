5f990d8e5088a4fe93ec0a384815bc19edd0df95
ngzax <ngzax@comcast.net>
2011-06-22 08:29:28 -0400

    * Added Google, MyOpenId, and Yahoo as Authentication Services. * Added link to log back in after log out * Changed from nickname to name for display since Google doesn't appear to support nickname

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, 'imkhJ6VxilZIA7AO36XpHg', 'FsFr1yuVI7RMp9O7v97j8dHkwSfFKeLMcgK74hyys', :sign_in => true
-end
+  require "openid/store/filesystem"
 
-# use OmniAuth::Strategies::LDAP, :host => '10.101.10.1', :port => 389, :method => :plain, :base => 'dc=intridea, dc=com', :uid => 'sAMAccountName'
+  provider :openid, OpenID::Store::Filesystem.new("./tmp"), :name => "google", :identifier => "https://www.google.com/accounts/o8/id"
+  provider :openid, OpenID::Store::Filesystem.new("./tmp"), :name => "myopenid", :identifier => "myopenid.com"
+  provider :openid, OpenID::Store::Filesystem.new("./tmp"), :name => "yahoo", :identifier => "yahoo.com"
+  
+  provider :twitter, 'imkhJ6VxilZIA7AO36XpHg', 'FsFr1yuVI7RMp9O7v97j8dHkwSfFKeLMcgK74hyys', :sign_in => true
+
+end
